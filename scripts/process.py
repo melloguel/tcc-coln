ce77eb320cb74bf34be6a0e030ecc0057a8167aa
Miguel de Mello Carpi <miguelmello@usp.br>
2022-06-23 23:46:58 -0300

    scripts: process: Simplificação da saída para csv

--- scripts/process.py
+++ scripts/process.py
@@ -2,7 +2,8 @@
 
 import argparse
 import re
-import os.path as path
+from pathlib import Path
+from os import path
 from statistics import NormalDist
 
 def conf_interval(data, confidence=0.95):
@@ -14,35 +15,31 @@ def conf_interval(data, confidence=0.95):
     dist = NormalDist.from_samples(data)
     z = NormalDist().inv_cdf((1 + confidence) / 2.)
     h = dist.stdev * z / ((len(data) - 1) ** .5)
-    return dist.mean - h, dist.mean + h, dist.mean
+    return dist.mean, h
 
 def pre_process(file_path, parse):
-    with open(file_path, 'r') as f:
-        contents = f.readlines()
+    with open(file_path, 'r') as fle:
+        contents = fle.readlines()
 
-    data = map(lambda s: re.search("Acurracy:\s+(.*)$", s), contents) if parse else contents
+    data = map(lambda s: re.search(r"Acurracy:\s+(.*)$", s), contents) if parse else contents
     data = filter(lambda i: i is not None, data)
     data = map(lambda m: float(m.groups()[0]), data) if parse else map(float, data)
     data = list(data)
 
     return data
 
-def main():
+if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Process test data.')
     parser.add_argument('--files', type=str, nargs='+',
                         help='Files to be processed')
     parser.add_argument('--no-preprocess', help='Preprocess the data first',
                         action='store_false')
-    parser.add_argument('-s', help='Omit file names', action='store_true')
 
     args = parser.parse_args()
-    # TODO: Setup CSV format to help formatting using column and tr or grep
     if args.files:
-        for f in args.files:
-            data = pre_process(path.abspath(f), parse=args.no_preprocess)
-            l, r, m  = conf_interval(data)
-            p = f"{f + ': ' if not args.s else '':<60}" if not args.s else ""
-            print(f"{p}{l:<10.4} {r:<10.4} {m:<10.4}")
-
-main()
+        print("test,mean,width")
+        for fle in args.files:
+            values = pre_process(Path(fle).absolute(), parse=args.no_preprocess)
+            mean, width  = conf_interval(values)
+            print(f"{fle},{mean:.3},{width:.3}")
