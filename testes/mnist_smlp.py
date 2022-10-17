ee797437e7ae83614549f7d1ffa9063c8f0b8398
Miguel de Mello Carpi <miguelmello@usp.br>
2022-10-17 10:17:38 -0300

    testes: mnist_smlp.py adicionado

--- testes/mnist_smlp.py
+++ testes/mnist_smlp.py
@@ -13,31 +13,34 @@ class SimpleMLP(nn.Module):
         self.linear2 = nn.Linear(100, 50)
         self.final   = nn.Linear(50, 10)
         self.relu    = nn.ReLU()
+        self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, image):
         a = image.view(-1, 28*28)
         a = self.relu(self.linear1(a))
         a = self.relu(self.linear2(a))
         a = self.final(a)
-        return a
+        output = self.softmax(a)
+        return output
 
 
-def mk_mnist_smlp(traindt, validdt, testdt, gpu):
+def mk_mnist_smlp(traindt, validdt, testdt, device):
     epochs       = 15
     criterion    = nn.CrossEntropyLoss()
     optim_params = { 'lr' : 0.01, 'momentum' : 0.9 }
     optimizer    = optim.SGD
 
 
-    return ModelConfig(SimpleMLP(),
-                       criterion,
-                       optimizer,
-                       optim_params,
-                       traindt,
-                       validdt,
-                       testdt,
-                       epochs=epochs,
-                       gpu=gpu)
+    return ModelConfig(
+            model=SimpleMLP(),
+            criterion=criterion,
+            optimizer=optimizer,
+            optimizer_params=optim_params,
+            traindt=traindt,
+            validdt=validdt,
+            testdt=testdt,
+            epochs=epochs,
+            device=device)
 
 if __name__ == '__main__':
     model = SimpleMLP()
